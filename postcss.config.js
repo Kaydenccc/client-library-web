33322602c3e8df405be4aa6b83f3b6f8eb0b5892
Kaydenccc <ffadilah682@gmail.com>
2022-11-24 20:49:32 +0800

    fixed27

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,8 @@
-module.exports = (ctx) => {
-  const plugins = {
-    tailwindcss: {},
-    autoprefixer: {
-      ...ctx.options.autoprefixer,
-      flexbox: 'no-2009',
-    },
-  };
-  return { plugins };
+module.exports = {
+  plugins: [
+    require('autoprefixer')({}),
+    require('tailwindcss')({}),
+    // tailwindcss: {},
+    // autoprefixer: {},
+  ],
 };
