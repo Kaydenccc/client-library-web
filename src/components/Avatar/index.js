05f9a622db27f0a8ab6817423b9334e4dd31d05c
Kaydenccc <ffadilah682@gmail.com>
2022-11-24 20:09:26 +0800

    fixed20

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -10,24 +10,43 @@ const Avatar = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   return (
-    <div
-      onClick={() => {
-        dispatch(setCloseSide());
-        navigate('/detail-user/' + user.user?._id);
-      }}
-      className={`w-full p-6 flex items-center gap-3 text-white cursor-pointer ${closeSide && 'visi-hidden'}`}
-    >
-      <div className="w-[80px] rounded-full overflow-hidden bg-slate-400">
-        <img className="w-full h-full object-cover" src={user.user?.image} alt={user.user?.username} />
+    <>
+      <div
+        onClick={() => {
+          dispatch(setCloseSide());
+          navigate('/detail-user/' + user.user?._id);
+        }}
+        className={`w-full p-6 flex block md:hidden items-center gap-3 text-white cursor-pointer ${closeSide && 'visi-hidden'}`}
+      >
+        <div className="w-[80px] rounded-full overflow-hidden bg-slate-400">
+          <img className="w-full h-full object-cover" src={user.user?.image} alt={user.user?.username} />
+        </div>
+        {/* <img className="w-[100px] rounded-full overflow-hidden" src="https://res.cloudinary.com/diqsivizd/image/upload/v1668267193/LIBRARY/user_image/pngegg_qhoull.png" alt="avatar" /> */}
+        <div className="w-full">
+          <p className="font-bold text-lg flex items-center m-0 ">
+            {user.user?.username} <FaUserEdit className="inline-block ml-3 " />
+          </p>
+          <p className="text-white/70 hover:text-white m-0 ">{user.user?.profesi}</p>
+        </div>
       </div>
-      {/* <img className="w-[100px] rounded-full overflow-hidden" src="https://res.cloudinary.com/diqsivizd/image/upload/v1668267193/LIBRARY/user_image/pngegg_qhoull.png" alt="avatar" /> */}
-      <div className="w-full">
-        <p className="font-bold text-lg flex items-center m-0 ">
-          {user.user?.username} <FaUserEdit className="inline-block ml-3 " />
-        </p>
-        <p className="text-white/70 hover:text-white m-0 ">{user.user?.profesi}</p>
+      <div
+        onClick={() => {
+          navigate('/detail-user/' + user.user?._id);
+        }}
+        className={`w-full p-6 hidden md:block flex items-center gap-3 text-white cursor-pointer ${closeSide && 'visi-hidden'}`}
+      >
+        <div className="w-[80px] rounded-full overflow-hidden bg-slate-400">
+          <img className="w-full h-full object-cover" src={user.user?.image} alt={user.user?.username} />
+        </div>
+        {/* <img className="w-[100px] rounded-full overflow-hidden" src="https://res.cloudinary.com/diqsivizd/image/upload/v1668267193/LIBRARY/user_image/pngegg_qhoull.png" alt="avatar" /> */}
+        <div className="w-full">
+          <p className="font-bold text-lg flex items-center m-0 ">
+            {user.user?.username} <FaUserEdit className="inline-block ml-3 " />
+          </p>
+          <p className="text-white/70 hover:text-white m-0 ">{user.user?.profesi}</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
